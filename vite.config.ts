948b5a4a92ef5c3e845bb02011cac808ac32d4a9
Igor <castromann@live.com>
2023-04-03 15:34:15 -0300

    chore: add alias path to src

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 import dns from "dns";
+import path from "path";
 
 dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@src": path.resolve(__dirname, "./src"),
+    },
+  },
 });
