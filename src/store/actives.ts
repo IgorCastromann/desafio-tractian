aa3629ff82002b4304346cc8c219c28c45ac0216
Igor <castromann@live.com>
2023-04-04 11:42:22 -0300

    fix(actives-store): make it observable and reactive

--- src/store/actives.ts
+++ src/store/actives.ts
@@ -1,11 +1,14 @@
 import { AnalysesService } from "@src/services/analyses";
 import { Active, Status } from "@src/services/types";
+import { makeAutoObservable } from "mobx";
 
 class ActivesStore {
   public actives: Active[] = [];
 
   // eslint-disable-next-line no-unused-vars
-  public constructor(private analysesService = new AnalysesService()) {}
+  public constructor(private analysesService = new AnalysesService()) {
+    makeAutoObservable(this);
+  }
 
   public fetchActives = async () => {
     const actives = await this.analysesService.fetchActives();
