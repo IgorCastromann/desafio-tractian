bb45b6f8db629525c04de26d1b3c5b37124fcda8
Igor <castromann@live.com>
2023-04-08 15:02:49 -0300

    chore(generic-chart): add solidGauge possibility

--- src/components/Charts/GenericChart/index.tsx
+++ src/components/Charts/GenericChart/index.tsx
@@ -2,12 +2,15 @@ import HighchartsReact from "highcharts-react-official";
 import { useRef } from "react";
 import Highcharts from "highcharts";
 import Exporting from "highcharts/modules/exporting";
+import highchartsMore from "highcharts/highcharts-more.js";
+import solidGauge from "highcharts/modules/solid-gauge.js";
 
+Exporting(Highcharts);
+highchartsMore(Highcharts);
+solidGauge(Highcharts);
 interface GenericChartProps {
   options: Highcharts.Options;
 }
-Exporting(Highcharts);
-
 export const GenericChart = ({ options }: GenericChartProps) => {
   const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
 
